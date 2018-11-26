b8d8a6a6263787eda30d31b6f20818b4c30bf0d8
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-11-25 20:02:18 -0700

    range 2

---
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/factories/LauncherConnectableObservable1.java
@@ -0,0 +1,36 @@
+package com.playing.rx.learningrxjava.factories;
+
+import io.reactivex.Observable;
+import io.reactivex.observables.ConnectableObservable;
+
+import java.util.concurrent.TimeUnit;
+
+public final class LauncherConnectableObservable1 {
+    public static void main(final String[] args) {
+        final ConnectableObservable<Long> seconds = Observable.interval(1, TimeUnit.SECONDS).publish();
+
+        // Cold emissions ... 
+
+        // Observer 1
+        seconds.subscribe(l -> System.out.println("Observer 1: " + l));
+        seconds.connect();
+
+        // sleep 5 seconds:
+        sleep(5000);
+
+        // Observer 2
+        seconds.subscribe(l -> System.out.println("Observer 2: " + l));
+        sleep(5000);
+
+
+    }
+
+    private static void sleep(final long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (final InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
--- rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/factories/ObservableInterval1.java
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/factories/ObservableInterval1.java
@@ -6,8 +6,25 @@ import java.util.concurrent.TimeUnit;
 
 public final class ObservableInterval1 {
     public static void main(final String[] args) throws InterruptedException {
-        Observable.interval(1, TimeUnit.SECONDS)
-                .subscribe(System.out::println);
-        Thread.sleep(TimeUnit.SECONDS.toMillis(5));
+        final Observable<Long> emits = Observable.interval(1, TimeUnit.SECONDS);
+
+        // Observer 1
+        emits.subscribe(lng -> System.out.println("Observer 1: " + lng));
+        sleep(5000);
+        //Thread.sleep(TimeUnit.MILLISECONDS.toSeconds(5000));
+
+        // Observer 2
+        emits.subscribe(lng -> System.out.println("Observer 2: " + lng));
+        sleep(5000);
+        //Thread.sleep(TimeUnit.MILLISECONDS.toSeconds(5000));
     }
+
+    private static void sleep(final long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (final InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
