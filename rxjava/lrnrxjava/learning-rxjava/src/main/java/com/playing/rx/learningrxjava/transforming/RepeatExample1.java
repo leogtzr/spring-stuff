8d8afe534b365ea46761b4405bb1c03651f0db6b
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-11-27 15:47:44 -0700

    repeat()

--- rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/transforming/RepeatExample1.java
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/transforming/RepeatExample1.java
@@ -3,13 +3,11 @@ package com.playing.rx.learningrxjava.transforming;
 import com.playing.rx.learningrxjava.util.SomeTools;
 import io.reactivex.Observable;
 
-import java.util.concurrent.TimeUnit;
-
-public final class DelayExample1 {
+public final class RepeatExample1 {
     public static void main(final String[] args) {
 
-        Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
-                .delay(3, TimeUnit.SECONDS)             // delay the emissions for 3 seconds
+        Observable.just("Leo", "Gtz", "Ramirez")
+                .repeat(3)
                 .subscribe(System.out::println);
 
         SomeTools.sleepSeconds(5L);
