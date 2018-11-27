4294e0436524563ad9454fe50e906b21847f3570
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-11-27 12:02:06 -0700

    more take() op examples.

--- rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/operators/suppressing/TakeExample1.java
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/operators/suppressing/TakeExample1.java
@@ -1,7 +1,55 @@
 package com.playing.rx.learningrxjava.operators.suppressing;
 
+import io.reactivex.Observable;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public final class TakeExample1 {
     public static void main(final String[] args) {
 
+        final List<String> elements = Arrays.asList("Alpha", "Beta", "Gamma", "Delta", "Epsilon");
+
+        Observable.interval(1, TimeUnit.SECONDS)
+                .take(elements.size())
+                .map(i -> elements.get(Math.toIntExact(i)))
+                .subscribe(System.out::println);
+
+        sleep(TimeUnit.SECONDS.toMillis(5L));
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>");
+
+        Observable.zip(
+                Observable.interval(1000, TimeUnit.MILLISECONDS).take(5),
+                Observable.fromArray(1, 2, 3, 4, 5),
+                (aLong, integer) -> integer
+                )
+                .subscribe(System.out::println);
+
+        sleep(TimeUnit.SECONDS.toMillis(10L));
+
+        System.out.println("==================================================");
+
+        Observable.zip(
+                Observable.interval(1, TimeUnit.SECONDS).take(3),
+                Observable.fromIterable(Arrays.asList("Mama", "Papa", "Edgar", "Brenda", "Leo")),
+                        (period, name) -> name
+                ).subscribe(System.out::println);
+
+        sleep(TimeUnit.SECONDS.toMillis(5L));
+
+
+
     }
+
+    private static void sleep(final long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (final InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
