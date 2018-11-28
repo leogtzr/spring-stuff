cde769e9935a05d5ed3543b35c9bd7ef2d5ca59f
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-11-27 21:17:37 -0700

    onError

---
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/error/OnErrorReturnItemExample1.java
@@ -0,0 +1,31 @@
+package com.playing.rx.learningrxjava.error;
+
+import com.playing.rx.learningrxjava.util.SomeTools;
+import io.reactivex.Observable;
+
+public class OnErrorReturnItemExample1 {
+    public static void main(final String[] args) {
+//        Observable.just(5, 2, 4, 0, 3, 2, 8)
+//                .map(i -> 10.0 / i)
+//                .onErrorReturnItem(-1.0)
+//                .subscribe(i -> System.out.println("Received: " + i),
+//                e -> System.out.println("Received error: " + e.getMessage())
+//                );
+
+//        Observable.just(5, 2, 4, 0, 3, 2, 8)
+//                .map(i -> 10 / i)
+//                .onErrorReturnItem(-1)
+//                .subscribe(i -> System.out.println("RECEIVED: " + i),
+//                        e -> System.out.println("RECEIVED ERROR: " + e)
+//                );
+
+        Observable.just(5, 2, 4, 0, 3, 2, 8)
+                .map(i -> 10 / i)
+                .onErrorReturn(e -> - 1)
+                .subscribe(i -> System.out.println("RECEIVED: " + i),
+                        e -> System.out.println("RECEIVED ERROR: " + e)
+                );
+
+        SomeTools.sleepSeconds(5L);
+    }
+}
