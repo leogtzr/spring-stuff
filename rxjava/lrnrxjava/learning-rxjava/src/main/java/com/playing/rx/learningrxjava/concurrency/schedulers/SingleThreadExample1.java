42d9b2e82f75ebf7e411f21603c8190d4808ccdb
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-11-30 13:23:41 -0700

    Schedulers.single()

--- rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/concurrency/schedulers/SingleThreadExample1.java
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/concurrency/schedulers/SingleThreadExample1.java
@@ -4,13 +4,14 @@ import com.playing.rx.learningrxjava.util.SomeTools;
 import io.reactivex.Observable;
 import io.reactivex.schedulers.Schedulers;
 
-public final class NewThreadSchedulerExample1 {
+public final class SingleThreadExample1 {
     public static void main(final String[] args) {
 
         Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
-                .subscribeOn(Schedulers.newThread())
+                .subscribeOn(Schedulers.single())
                 .map(SomeTools::intenseCalculation)
-                .blockingSubscribe(System.out::println, Throwable::printStackTrace, () -> System.out.println("Done!"));
+                .subscribe(System.out::println)
+                ;
 
     }
 }
