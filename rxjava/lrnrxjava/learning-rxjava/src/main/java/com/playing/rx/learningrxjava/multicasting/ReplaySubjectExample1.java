9d0714be840dcaf44f45c993ac020eb52e8347e8
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-11-29 21:00:45 -0700

    ReplaySubjectExample

--- rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/multicasting/ReplaySubjectExample1.java
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/multicasting/ReplaySubjectExample1.java
@@ -1,21 +1,25 @@
 package com.playing.rx.learningrxjava.multicasting;
 
 import com.playing.rx.learningrxjava.util.SomeTools;
-import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.ReplaySubject;
 import io.reactivex.subjects.Subject;
 
-public final class BehaviorSubjectExample1 {
+public final class ReplaySubjectExample1 {
     public static void main(final String[] args) {
-        final Subject<String> subject = BehaviorSubject.create();
+
+        final Subject<String> subject = ReplaySubject.create();
 
         subject.subscribe(s -> System.out.println("Observer 1: " + s));
 
         subject.onNext("Alpha");
         subject.onNext("Beta");
         subject.onNext("Gamma");
+        subject.onComplete();
 
+        // The following observer receives all the previous emissions:
         subject.subscribe(s -> System.out.println("Observer 2: " + s));
 
-        SomeTools.sleepSeconds(5L);
+        SomeTools.sleepSeconds(3L);
+
     }
 }
