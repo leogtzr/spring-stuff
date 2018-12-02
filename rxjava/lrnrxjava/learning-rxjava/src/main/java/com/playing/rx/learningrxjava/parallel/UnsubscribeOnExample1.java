d3d367a14c39ef343cc1ed77dfd871e8ea169ffd
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-12-01 17:36:31 -0700

    disposing on a separate thread

--- rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/parallel/UnsubscribeOnExample1.java
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/parallel/UnsubscribeOnExample1.java
@@ -3,20 +3,20 @@ package com.playing.rx.learningrxjava.parallel;
 import com.playing.rx.learningrxjava.util.SomeTools;
 import io.reactivex.Observable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 import java.util.concurrent.TimeUnit;
 
-public final class DisposingExample1 {
+public final class UnsubscribeOnExample1 {
     public static void main(final String[] args) {
-
         final Disposable d = Observable.interval(1, TimeUnit.SECONDS)
-                .doOnDispose(() -> System.out.println("Disposing on thread " + Thread.currentThread().getName()))
+                .doOnDispose(() -> System.out.println("Dispose on thread " + Thread.currentThread().getName()))
+                .unsubscribeOn(Schedulers.io())         // Disposal is being done by the IO Scheduler.
                 .subscribe(i -> System.out.println("Received " + i))
                 ;
 
         SomeTools.sleepSeconds(3L);
         d.dispose();
         SomeTools.sleepSeconds(3L);
-
     }
 }
