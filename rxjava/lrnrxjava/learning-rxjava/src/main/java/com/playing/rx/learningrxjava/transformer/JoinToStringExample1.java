35c5e86270a6d5309a8f1e665dcb85972e0a3fae
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-12-05 09:40:00 -0700

    adding joinToString example

--- rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/transformer/JoinToStringExample1.java
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/transformer/JoinToStringExample1.java
@@ -1,4 +1,23 @@
 package com.playing.rx.learningrxjava.transformer;
 
-public class JoinToStringExample1 {
+import io.reactivex.Observable;
+import io.reactivex.ObservableTransformer;
+
+public final class JoinToStringExample1 {
+    public static void main(final String[] args) {
+        Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+                .compose(joinToString("/"))
+                .subscribe(System.out::println)
+                ;
+    }
+
+    private static ObservableTransformer<String, String>  joinToString(final String separator) {
+        return stream -> stream.collect(StringBuilder::new, (b, s) -> {
+            if (b.length() == 0)
+                b.append(s);
+            else
+                b.append(separator).append(s);
+        }).map(StringBuilder::toString)
+                .toObservable();
+    }
 }
--- rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/transformer/ObservableTransformerExample1.java
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/transformer/ObservableTransformerExample1.java
@@ -1,4 +1,20 @@
 package com.playing.rx.learningrxjava.transformer;
 
-public class ObservableTransformerExample1 {
+import com.google.common.collect.ImmutableList;
+import io.reactivex.Observable;
+import io.reactivex.ObservableTransformer;
+
+public final class ObservableTransformerExample1 {
+    public static void main(final String[] args) {
+        Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+                .compose(toImmutableList())
+                .subscribe(System.out::println)
+                ;
+    }
+
+    private static <T> ObservableTransformer<T, ImmutableList<T>> toImmutableList() {
+        return  upstream -> upstream.collect(ImmutableList::<T>builder, ImmutableList.Builder::add)
+                .map(ImmutableList.Builder::build)
+                .toObservable(); // must turn Single into Observable
+    }
 }
