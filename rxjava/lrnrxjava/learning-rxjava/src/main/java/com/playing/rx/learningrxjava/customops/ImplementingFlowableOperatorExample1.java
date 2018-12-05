a0dc3b9db0e9241bfa975c55dd93169e660b6888
leogtzr <leogutierrezramirez@gmail.com>
2018-12-05 12:04:04 -0700

    flowable operator

---
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/customops/ImplementingFlowableOperatorExample1.java
@@ -0,0 +1,51 @@
+package com.playing.rx.learningrxjava.customops;
+
+import io.reactivex.Flowable;
+import io.reactivex.FlowableOperator;
+import io.reactivex.functions.Action;
+import io.reactivex.subscribers.DisposableSubscriber;
+
+public final class ImplementingFlowableOperatorExample1 {
+
+    public static void main(String[] args) {
+        Flowable.range(1, 5)
+                .lift(doOnEmpty(() -> System.out.println("Operation 1 Empty!")))
+                .subscribe(v -> System.out.println("Operation 1: " + v));
+
+        Flowable.<Integer>empty()
+                .lift(doOnEmpty(() -> System.out.println("Operation 2 Empty!")))
+                .subscribe(v -> System.out.println("Operation 2: " + v));
+    }
+
+    private static <T> FlowableOperator<T, T> doOnEmpty(final Action action) {
+        return observer -> new DisposableSubscriber<T>() {
+
+            private boolean isEmpty = true;
+
+            @Override
+            public void onNext(final T value) {
+                this.isEmpty = false;
+                observer.onNext(value);
+            }
+
+            @Override
+            public void onError(final Throwable throwable) {
+                observer.onError(throwable);
+            }
+
+            @Override
+            public void onComplete() {
+                if (this.isEmpty) {
+                    try {
+                        action.run();
+                    } catch (final Exception ex) {
+                        onError(ex);
+                        return;
+                    }
+                }
+                observer.onComplete();
+            }
+        };
+    }
+
+}
