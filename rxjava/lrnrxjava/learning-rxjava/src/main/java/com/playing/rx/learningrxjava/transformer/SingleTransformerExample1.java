00d6dd3abc4c0f6e0bcc81d2c8469391896ee2c0
leogtzr <leogutierrezramirez@gmail.com>
2018-12-06 13:16:08 -0700

    SingleTransformer

---
+++ rxjava/lrnrxjava/learning-rxjava/src/main/java/com/playing/rx/learningrxjava/transformer/SingleTransformerExample1.java
@@ -0,0 +1,23 @@
+package com.playing.rx.learningrxjava.transformer;
+
+import com.playing.rx.learningrxjava.sideffects.SharingStateIssueExample1;
+import io.reactivex.Observable;
+import io.reactivex.SingleTransformer;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public final class SingleTransformerExample1 {
+
+    public static void main(final String[] args) {
+        Observable.just("alpha", "Beta", "Gamma", "Delta", "Epsilon")
+                .toList()
+                .compose(toUnmodifiable())
+                .subscribe(System.out::println);
+    }
+
+    private static <T> SingleTransformer<Collection<T>, Collection<T>> toUnmodifiable() {
+        return singleObserver -> singleObserver.map(Collections::unmodifiableCollection);
+    }
+
+}
