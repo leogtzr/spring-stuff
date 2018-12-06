b57acc61c5020a4731cf084a369b85efb3f519a6
leogtzr <leogutierrezramirez@gmail.com>
2018-12-06 14:14:24 -0700

    blocking Subscribe

---
+++ rxjava/lrnrxjava/learning-rxjava/src/test/java/com/playing/rx/learningrxjava/blocking/TestBlockingSubscribeExample1.java
@@ -0,0 +1,25 @@
+package com.playing.rx.learningrxjava.blocking;
+
+import io.reactivex.Observable;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class TestBlockingSubscribeExample1 {
+
+    @Test
+    public void testBlockingSubscribe() {
+        final AtomicInteger hitCount = new AtomicInteger();
+        final Observable<Long> source = Observable.interval(1, TimeUnit.SECONDS)
+                .take(5);
+
+        source.blockingSubscribe(i -> hitCount.incrementAndGet());
+
+        assertEquals(5, hitCount.get());
+    }
+
+}
