41bd7f15ab617eb147b6f4518beb571dd3e98d4e
leogtzr <leogutierrezramirez@gmail.com>
2018-12-13 10:35:35 -0700

    fixing dependency scope

--- spring-data/data-playing/src/main/java/com/learning/data/dataplaying/DataPlayingApplication.java
+++ spring-data/data-playing/src/main/java/com/learning/data/dataplaying/DataPlayingApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DataPlayingApplication {
-
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(DataPlayingApplication.class, args);
 	}
-
 }
 
