2d61d2f59881e8e6606f7aa47b0c0475179970c2
leogtzr <leogutierrezramirez@gmail.com>
2018-12-13 09:48:24 -0700

    hsgdjf

---
+++ spring-data/data-playing/src/main/java/com/learning/data/dataplaying/DataPlayingApplication.java
@@ -0,0 +1,14 @@
+package com.learning.data.dataplaying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPlayingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPlayingApplication.class, args);
+	}
+
+}
+
