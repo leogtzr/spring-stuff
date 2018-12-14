871cbb883f4dce93d352c47be4c5ea241ebd20a9
leogtzr <leogutierrezramirez@gmail.com>
2018-12-14 12:37:15 -0700

    some config to the actuator endpoints.

--- rxjava/reactor-playing/src/main/java/com/reactive/playing/reactorplaying/ReactorPlayingApplication.java
+++ rxjava/reactor-playing/src/main/java/com/reactive/playing/reactorplaying/ReactorPlayingApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactorPlayingApplication {
-
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(ReactorPlayingApplication.class, args);
 	}
-
 }
 
