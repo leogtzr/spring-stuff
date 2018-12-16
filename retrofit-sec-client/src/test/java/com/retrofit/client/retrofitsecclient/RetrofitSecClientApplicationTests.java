de9cd9c730034d2e7d030c47484355658db6fb4d
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-12-15 19:22:03 -0700

    adding initial project to try retrofit

---
+++ retrofit-sec-client/src/test/java/com/retrofit/client/retrofitsecclient/RetrofitSecClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.retrofit.client.retrofitsecclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RetrofitSecClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
