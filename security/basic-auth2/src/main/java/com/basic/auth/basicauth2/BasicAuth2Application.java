b4437fc1f9e722b9f3d51dea8b074eb5c0f2f046
leogtzr <leogutierrezramirez@gmail.com>
2018-12-18 10:43:33 -0700

    basic auth with inmemory db

---
+++ security/basic-auth2/src/main/java/com/basic/auth/basicauth2/BasicAuth2Application.java
@@ -0,0 +1,12 @@
+package com.basic.auth.basicauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuth2Application {
+	public static void main(final String[] args) {
+		SpringApplication.run(BasicAuth2Application.class, args);
+	}
+}
+
---
+++ security/basic-auth2/src/test/java/com/basic/auth/basicauth2/BasicAuth2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.basic.auth.basicauth2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicAuth2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
