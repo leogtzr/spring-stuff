d44074b5ac4d30300c6d49683348a5c74b72c562
leogtzr <leogutierrezramirez@gmail.com>
2018-12-19 14:02:50 -0700

    adding simple batch project

---
+++ batch/batch-playing/src/main/java/com/batch/playing/batchplaying/BatchPlayingApplication.java
@@ -0,0 +1,14 @@
+package com.batch.playing.batchplaying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPlayingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchPlayingApplication.class, args);
+	}
+
+}
+
