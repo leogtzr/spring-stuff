0a5786f50d1e26159fde964b66e71e38d9390ec1
leogtzr <leogutierrezramirez@gmail.com>
2018-12-19 14:13:00 -0700

    adding simple code to execute the jobs.

--- batch/batch-playing/src/main/java/com/batch/playing/batchplaying/BatchPlayingApplication.java
+++ batch/batch-playing/src/main/java/com/batch/playing/batchplaying/BatchPlayingApplication.java
@@ -1,12 +1,49 @@
 package com.batch.playing.batchplaying;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@Configuration
 public class BatchPlayingApplication {
 
-	public static void main(String[] args) {
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job job(final Step ourBatchStep) throws Exception {
+		return jobBuilderFactory.get("jobLeo")
+				.incrementer(new RunIdIncrementer())
+				.start(ourBatchStep)
+				.build()
+				;
+	}
+
+	@Bean
+	public Step ourBatchStep() {
+		return stepBuilderFactory.get("stepLeo")
+				.tasklet((stepContribution, chunkContext) -> null)
+				.build();
+	}
+
+	public static void main(final String[] args) {
 		SpringApplication.run(BatchPlayingApplication.class, args);
 	}
 
