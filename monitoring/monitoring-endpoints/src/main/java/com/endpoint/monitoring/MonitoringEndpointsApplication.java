970e51d66840dd9a1907a5a5f312e8305870ef41
Leo Gutiérrez Ramírez <leogutierrezramirez@gmail.com>
2018-12-29 17:12:54 -0700

    adding small project to test actuator

---
+++ monitoring/monitoring-endpoints/src/main/java/com/endpoint/monitoring/MonitoringEndpointsApplication.java
@@ -0,0 +1,12 @@
+package com.endpoint.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringEndpointsApplication {
+	public static void main(final String[] args) {
+		SpringApplication.run(MonitoringEndpointsApplication.class, args);
+	}
+}
+
