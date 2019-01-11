e1eeb794d50205d7e177ddb54e8d35eb4687c2f5
Leo <leogutierrezramirez@gmail.com>
2019-01-11 04:35:16 -0700

    adding projects

---
+++ monitoring/app-shutdown/src/main/java/com/az/monitoring/appshutdown/AppShutdownApplication.java
@@ -0,0 +1,14 @@
+package com.az.monitoring.appshutdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppShutdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppShutdownApplication.class, args);
+	}
+
+}
+
