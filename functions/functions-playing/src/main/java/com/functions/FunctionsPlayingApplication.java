4e64b6623f42d86b20d5df3c1b0e9fcc3af2d5b6
Leo <leogutierrezramirez@gmail.com>
2019-01-15 11:13:15 -0700

    notes

--- functions/functions-playing/src/main/java/com/functions/FunctionsPlayingApplication.java
+++ functions/functions-playing/src/main/java/com/functions/FunctionsPlayingApplication.java
@@ -3,6 +3,7 @@ package com.functions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @FunctionScan
 @SpringBootApplication
 public class FunctionsPlayingApplication {
 	public static void main(final String[] args) {
